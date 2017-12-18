15da3459ee965fffe6e8748821988fd772a51287
Don Undeen <dhu3@georgetown.edu>
2017-12-18 17:01:11 +0000

    make backup files when copying over new configs

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # commands to setup kiosk mode hopefully you can just run this file, but you might need to do some other stuff too.
+
 # get the clock set right
 # git clone this project
 # git clone https://github.com/GeorgetownMakerHubOrg/lenstable.git
@@ -19,8 +20,10 @@ chmod a+x /home/pi/lenstable/gitupdate.sh
 
 
 # copy wpa_supplicant and interfaces files into place
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak
 sudo cp etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 # then edit this file for your specific network needs
+sudo cp  /etc/network/interfaces /etc/network/interfaces.bak
 sudo cp etc/network/interfaces /etc/network/interfaces
 
 #install network_up
@@ -36,8 +39,9 @@ sudo ntpdate -u time.nist.gov
 sudo apt-get install chromium-browser x11-xserver-utils unclutter
 
 # copy lxsession config file
-sudo cp home/pi/dot_config/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 # then edit autostart for the html file you want to display
+sudo cp /home/pi/.config/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/autostart.bak
+sudo cp home/pi/dot_config/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 
 
 #install mlocate

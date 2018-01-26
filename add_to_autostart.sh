87ee6bf18c8ec3de0758f50652e16eb559fcb6f1
don undeen <dhu3@georgetown.edu>
2018-01-26 16:54:52 -0500

    improvements

--- add_to_autostart.sh
+++ add_to_autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences
-sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
+sed -i 's/"exited_cleanly":[ ]*false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":[ ]*"Crashed"/"exit_type":"None"/' ~/.config/chromium/Default/Preferences
